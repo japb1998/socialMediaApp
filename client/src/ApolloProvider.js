cf12435522dd6e797dc636f434940d24e375b9bf
Javier <javierperez@Javiers-MBP.lan>
2021-05-29 14:33:23 -0400

    creating a react app with an apolloClient and provider setup

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import App from './App';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+const client = new ApolloClient({
+  uri: 'http://localhost:5000',
+  cache: new InMemoryCache()
+});
+
+export default (
+    <ApolloProvider><App/></ApolloProvider>
+)
